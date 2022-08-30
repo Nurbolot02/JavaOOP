45a18b7fb40e58f767abd508da52c1179f496879
Nurbolot <ngulamidinov@gmail.com>
2022-08-30 19:17:34 +0600

    Lesson1 add Geo

--- src/Lesson1/Geo/Person.java
+++ src/Lesson1/Geo/Person.java
@@ -1,5 +1,7 @@
 package Lesson1.Geo;
 
+import java.util.Objects;
+
 public class Person {
     public String fullName;
 
@@ -11,4 +13,16 @@ public class Person {
         return fullName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(fullName, person.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName);
+    }
 }
