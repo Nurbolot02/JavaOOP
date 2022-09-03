6ffb80f18814fd3df7ce8f6ac23dcc58c141cb2e
Nurbolot <ngulamidinov@gmail.com>
2022-09-03 21:16:09 +0600

    Lesson1/Geo - update GeoTree

--- src/Lesson1/Geo/GeoTree.java
+++ src/Lesson1/Geo/GeoTree.java
@@ -9,10 +9,26 @@ public class GeoTree {
         return tree;
     }
 
-    public void append(Person parent, Person children) {
-        if (!parent.equals(children)){
-            tree.add(new Node(parent, Relationship.parent, children));
-            tree.add(new Node(children, Relationship.children, parent));
+    public void append(Person person1, Person person2, Relationship relationship) {
+        if (!person1.equals(person2)) {
+            switch (relationship) {
+                case PARENT:
+                    tree.add(new Node(person1, Relationship.PARENT, person2));
+                    tree.add(new Node(person2, Relationship.CHILD, person1));
+                    break;
+                case GRANDPA, GRANDMA:
+                    tree.add(new Node(person1, (person1.getGender().equals(Gender.MALE) ? Relationship.GRANDPA : Relationship.GRANDMA), person2));
+                    tree.add(new Node(person2, Relationship.GRAND_CHILD, person1));
+                    break;
+                case CHILD:
+                    tree.add(new Node(person1, Relationship.CHILD, person2));
+                    tree.add(new Node(person2, Relationship.PARENT, person1));
+                    break;
+                case GRAND_CHILD:
+                    tree.add(new Node(person1, (person1.getGender().equals(Gender.MALE) ? Relationship.GRANDPA : Relationship.GRANDMA), person2));
+                    tree.add(new Node(person2, (person1.getGender().equals(Gender.MALE) ? Relationship.GRANDPA : Relationship.GRANDMA), person1));
+            }
+
         }
     }
 
