06d8d7dd3eb9a97b372f921e04708688e1dd68c5
Nurbolot <ngulamidinov@gmail.com>
2022-09-03 21:18:07 +0600

    Lesson1/Geo - update Person

--- src/Lesson1/Geo/Person.java
+++ src/Lesson1/Geo/Person.java
@@ -1,21 +1,25 @@
 package Lesson1.Geo;
 
 
-
 import Lesson1.Geo.Animal.Animal;
-import Lesson1.Geo.Animal.Communication;
 
 import java.util.Objects;
 
-public class Person implements Communication {
-    public String fullName;
+public class Person implements Communication, SearchRelatives {
+    private String fullName;
+    private Gender gender;
 
-    public Person(String fullName) {
+    public Person(String fullName, Gender gender) {
         this.fullName = fullName;
+        this.gender = gender;
     }
 
     public String getFullName() {
         return fullName;
+    }
+
+    public Gender getGender() {
+        return this.gender;
     }
 
     @Override
@@ -33,6 +37,11 @@ public class Person implements Communication {
 
     @Override
     public void call(Animal animal) {
-        System.out.printf("%s called %s, %s said %s\n", this.fullName, animal.getName() , animal.getName(), animal.voice());
+        System.out.printf("%s called %s, %s said %s\n", this.fullName, animal.getName(), animal.getName(), animal.voice());
+    }
+
+    @Override
+    public void play(Person person) {
+        System.out.printf("%s play with %s \n", this.getFullName(), person.fullName);
     }
 }
