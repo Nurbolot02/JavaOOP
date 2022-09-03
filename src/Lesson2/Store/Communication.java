75533e905f35f996ce802c93405c3ad9a6000c89
Nurbolot <ngulamidinov@gmail.com>
2022-09-04 00:18:47 +0600

    Lesson2/Store - add Communication

--- src/Lesson2/Store/Communication.java
+++ src/Lesson2/Store/Communication.java
@@ -1,41 +1,41 @@
 package Lesson2.Store;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 public interface Communication {
 
-    default double costOfItem(Item item){
+    default double costOfItem(Item item) {
         return item.getCost();
     }
 
-    default void isItemsInStorage(Seller seller, List<Item> wishListOfConsumer){
+    default void isItemsInStorage(Seller seller, List<Item> wishListOfConsumer) {
         for (Item item : wishListOfConsumer) {
-            if(!seller.getItems().contains(item)){
-                System.out.println("У нас нет " + item.getNameOfItem() );
+            if (!seller.getItems().containsKey(item)) {
+                System.out.println("У нас нет " + item.getNameOfItem());
             }
         }
     }
 
     //Дописать метод
-    default double totalCost(ArrayList<Item> items){
+    default double totalCost(TreeMap<NameOfProducts, Item> items) {
         double resCost = 0;
-        for (Item item : items){
+        for (Item item : items.values()) {
+            System.out.println(item.getNameOfItem() + " " + item.getCost());
             resCost += costOfItem(item);
         }
         System.out.println("final cost: " + resCost);
         return resCost;
     }
 
-    default void areYouPoor (Consumer consumer){
-        if(totalCost(consumer.getBucket()) > consumer.getMoney()){
+    default boolean areYouPoor() {
+        if (totalCost(((Consumer) this).getBucket()) > ((Consumer) this).getMoney()) {
             System.out.println("Ты нищеброд");
+            return true;
         } else {
-            walkInStorage();
+            return false;
         }
     }
 
-
-    //У Покупателя и Продавца реализовать свои вариации метода "пойти на склад"
     void walkInStorage();
 }
