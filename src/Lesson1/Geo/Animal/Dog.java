ee2215f83fb41fdef55a6bd9573f4b111e7c3125
Nurbolot <ngulamidinov@gmail.com>
2022-09-03 21:13:47 +0600

    Lesson1/Geo - update Dog

--- src/Lesson1/Geo/Animal/Dog.java
+++ src/Lesson1/Geo/Animal/Dog.java
@@ -1,6 +1,10 @@
 package Lesson1.Geo.Animal;
 
-public class Dog extends Animal{
+public class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
     @Override
     public String voice() {
         return "гав-гав";
