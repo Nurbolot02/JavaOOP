31c7d6ca94f6129e0852e84ec0f0eca42d990aab
Nurbolot <ngulamidinov@gmail.com>
2022-09-04 00:19:20 +0600

    Lesson2/Store - add GetInfo

--- src/Lesson2/Store/GetInfo.java
+++ src/Lesson2/Store/GetInfo.java
@@ -1,11 +1,11 @@
 package Lesson2.Store;
 
 public interface GetInfo {
-    default void showInfo(Human human){
-        System.out.println("Name: " + human.getName());
-        System.out.println("money: " + human.getMoney());
-        System.out.println("loveIndex: " + human.getLoveIndex());
+    default void showInfo() {
+        System.out.println((((Human) this).getClass().getSimpleName().equals("Seller") ? "Seller" : "Consumer"));
+        System.out.printf("Name: %s\n", ((Human) this).getName());
+        System.out.println("money: " + ((Human) this).getMoney());
+        System.out.println("loveIndex: " + ((Human) this).getLoveIndex());
         System.out.println();
-
     }
 }
