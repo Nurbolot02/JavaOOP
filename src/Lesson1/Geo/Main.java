8781b8c94ab0457e1ab72fbbf8ce751ad45fa3f1
Nurbolot <ngulamidinov@gmail.com>
2022-09-03 21:22:12 +0600

    Lesson1/Geo - update - Main

--- src/Lesson1/Geo/Main.java
+++ src/Lesson1/Geo/Main.java
@@ -5,29 +5,35 @@ import Lesson1.Geo.Animal.Dog;
 
 public class Main {
     public static void main(String[] args) {
-        Person irina = new Person("Ирина");
-        Person vasya = new Person("Вася");
-        Person masha = new Person("Маша");
-        Person jane = new Person("Женя");
-        Person ivan = new Person("Ваня");
+        Person irina = new Person("Ирина", Gender.FEMALE);
+        Person vasya = new Person("Вася", Gender.MALE);
+        Person masha = new Person("Маша", Gender.FEMALE);
+        Person jane = new Person("Женя", Gender.FEMALE);
+        Person ivan = new Person("Иван", Gender.MALE);
+        Person jek = new Person("jek", Gender.MALE);
         GeoTree gt = new GeoTree();
-        gt.append(irina, vasya);
-        gt.append(irina, masha);
-        gt.append(vasya, jane);
-        gt.append(vasya, ivan);
 
-        gt.append(vasya, irina);
-        gt.append(vasya, vasya);
+        gt.append(irina, vasya, Relationship.PARENT);
+        gt.append(irina, masha, Relationship.PARENT);
+        gt.append(vasya, jane, Relationship.PARENT);
+        gt.append(vasya, ivan, Relationship.PARENT);
+        gt.append(irina, ivan, Relationship.PARENT);
+        gt.append(jek, masha, Relationship.GRANDPA);
 
-        Cat cat = new Cat("cat", 2);
-        Dog dog = new Dog();
+        Cat cat = new Cat("musya", 2);
+        Dog dog = new Dog("Barsik", 4);
         vasya.call(cat);
         irina.call(dog);
 
-        System.out.println(new Research(gt).spend(irina,
-                Relationship.parent));
-        System.out.println(new Research(gt).spend(vasya,
-                Relationship.parent));
+        irina.searchRelatives(gt, Relationship.CHILD);
+        irina.searchRelatives(gt, Relationship.PARENT);
+        ivan.searchRelatives(gt, Relationship.PARENT);
+        jek.getAllRelatives(gt);
+        gt.append(jek, irina, Relationship.GRANDPA);
+        gt.append(jane, irina, Relationship.GRANDMA);
+        irina.getAllRelatives(gt);
+        jane.getAllRelatives(gt);
+        masha.play(ivan);
     }
 
 }
