d68092b1a3aae431189ef3b9e14f6f3f237beb52
Nurbolot <ngulamidinov@gmail.com>
2022-09-04 19:59:51 +0600

    Lesson1/Geo - update Animal.java - add method setIndex override hasNext and next

--- src/Lesson1/Geo/Animal/Animal.java
+++ src/Lesson1/Geo/Animal/Animal.java
@@ -1,8 +1,14 @@
 package Lesson1.Geo.Animal;
 
-public abstract class Animal {
+import java.util.Iterator;
+
+public abstract class Animal implements Iterator<String>, GetInfo {
     private String name;
     private int age;
+    private int index;
+    {
+        index = 0;
+    }
 
     public Animal(String name, int age) {
         this.name = name;
@@ -26,5 +32,25 @@ public abstract class Animal {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index++ < 2;
+    }
+
+    @Override
+    public String next() {
+        switch (index){
+            case 1:
+                return name;
+            case 2:
+                return String.valueOf(age);
+        }
+        return null;
     }
 }
