778f7c10eff4281aa7d00bd62c1b8fc728b87cab
Nurbolot <ngulamidinov@gmail.com>
2022-09-04 20:55:56 +0600

    Lesson2/Store - update GetInfo.java

--- src/Lesson2/Store/GetInfo.java
+++ src/Lesson2/Store/GetInfo.java
@@ -1,11 +1,20 @@
 package Lesson2.Store;
 
+import Lesson1.Geo.Person;
+
 public interface GetInfo {
     default void showInfo() {
         System.out.println((((Human) this).getClass().getSimpleName().equals("Seller") ? "Seller" : "Consumer"));
         System.out.printf("Name: %s\n", ((Human) this).getName());
         System.out.println("money: " + ((Human) this).getMoney());
         System.out.println("loveIndex: " + ((Human) this).getLoveIndex());
+        System.out.println();
+    }
+    default void showInfoIterator() {
+        ((Human) this).setIndex(0);
+        while (((Human) this).hasNext()) {
+            System.out.print(((Human) this).next() + " ");
+        }
         System.out.println();
     }
 }
