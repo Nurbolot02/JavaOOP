8cb38c7d6a152e0778c927ea530c6d6242d18fb4
Nurbolot <ngulamidinov@gmail.com>
2022-09-04 23:57:23 +0600

    Lesson4/Example - Ex005

---
+++ src/Lesson4/Example/Ex005/Ex004_GenericTest.java
@@ -0,0 +1,10 @@
+package Lesson4.Example.Ex005;
+
+public class Ex004_GenericTest {
+    public static void main(String[] args) {
+        // T type
+        // E element
+        // K key
+        // V value
+    }
+}
